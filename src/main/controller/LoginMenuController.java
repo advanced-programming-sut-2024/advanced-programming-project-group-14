d66ff5fd076a9aa8d7758d00c41fc37443a83fbf
amirhossein <a.h.yeganeh62722@gmail.com>
2024-06-16 08:32:49 +0330

    reset login controller and menu to default

--- src/main/controller/LoginMenuController.java
+++ src/main/controller/LoginMenuController.java
@@ -8,42 +8,6 @@ import javafx.scene.control.TextField;
 import main.model.Result;
 
 public class LoginMenuController {
-    @FXML
-    private PasswordField passwordField;
-    @FXML
-    private TextField passwordTextField;
-    @FXML
-    private Button togglePasswordVisibilityButton;
-    @FXML
-    private PasswordField confirmPasswordField;
-    @FXML
-    private TextField confirmPasswordTextField;
-    @FXML
-    private Button toggleConfirmPasswordVisibilityButton;
-
-    @FXML
-    public void initialize() {
-//        togglePasswordVisibilityButton.setOnAction(event -> togglePasswordVisibility(passwordField, togglePasswordVisibilityButton, passwordTextField));
-//        passwordField.textProperty().bindBidirectional(passwordTextField.textProperty());
-//        toggleConfirmPasswordVisibilityButton.setOnAction(event -> togglePasswordVisibility(confirmPasswordField, toggleConfirmPasswordVisibilityButton, confirmPasswordTextField));
-//        confirmPasswordField.textProperty().bindBidirectional(confirmPasswordTextField.textProperty());
-    }
-
-    private void togglePasswordVisibility(PasswordField field, Button button, TextField textField) {
-        if (field.isVisible()) {
-            field.setVisible(false);
-            field.setManaged(false);
-            textField.setVisible(true);
-            textField.setManaged(true);
-            button.setText("🙈");
-        } else {
-            field.setVisible(true);
-            field.setManaged(true);
-            textField.setVisible(false);
-            textField.setManaged(false);
-            button.setText("👁");
-        }
-    }
 
     public static Result register(String username, String password, String passwordConfirm, String nickname, String email) {
 
