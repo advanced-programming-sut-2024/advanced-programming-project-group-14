2437489ced06d1c4e888d07540803c21343f12dd
amirhossein <a.h.yeganeh62722@gmail.com>
2024-06-21 16:49:37 +0330

    New password dialog added

--- src/main/view/LoginMenuView.java
+++ src/main/view/LoginMenuView.java
@@ -3,10 +3,12 @@ package main.view;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 
@@ -21,7 +23,7 @@ public class LoginMenuView extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/resources/FXML/RegisterMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/resources/FXML/LoginMenu.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/resources/CSS/gwent-theme.css").toExternalForm());
         primaryStage.setScene(scene);
@@ -29,19 +31,16 @@ public class LoginMenuView extends Application {
         primaryStage.setHeight(600);
         primaryStage.setWidth(800);
         primaryStage.show();
-        showDialog();
     }
 
-    private void showDialog(){
+    private void showQuestionDialog(){
         Dialog<Pair<String, String>> dialog = new Dialog<>();
         dialog.setTitle("Questions");
         dialog.setHeaderText("Please choose a question and answer it:");
 
-        // Set the button types
         ButtonType OkType = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
         dialog.getDialogPane().getButtonTypes().addAll(OkType, ButtonType.CANCEL);
 
-        // Create the username and password labels and fields
         GridPane grid = new GridPane();
         grid.setHgap(10);
         grid.setVgap(10);
@@ -71,7 +70,6 @@ public class LoginMenuView extends Application {
         dialog.setTitle("dialog-title");
 
 
-        // Convert the result to a pair of question and answer when the OK button is clicked
         dialog.setResultConverter(dialogButton -> {
             if (dialogButton == OkType) {
                 return new Pair<>(questionComboBox.getValue(), answerField.getText());
@@ -82,6 +80,58 @@ public class LoginMenuView extends Application {
         dialog.showAndWait().ifPresent(result -> {
             //ToDo:
             // save questions with the answers(result.getKey and result.getValue)
+        });
+    }
+
+    private void showNewPasswordDialog(){
+        Dialog<String> dialog = new Dialog<>();
+        dialog.setTitle("New Password");
+        dialog.setHeaderText("Please enter your new password:");
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        ButtonType OkButton = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(OkButton, ButtonType.CANCEL);
+        Label passwordLabel = new Label("Password:");
+        TextField passwordField = new TextField();
+        Label confirmPasswordLabel = new Label("Confirm password:");
+        TextField confirmPasswordField = new TextField();
+
+        grid.add(passwordLabel, 0, 0);
+        grid.add(passwordField, 1, 0);
+        grid.add(confirmPasswordLabel, 0, 1);
+        grid.add(confirmPasswordField, 1, 1);
+
+        dialog.getDialogPane().setContent(grid);
+        dialog.getDialogPane().getStylesheets().add(getClass().getResource("/resources/CSS/gwent-theme.css").toExternalForm());
+        dialog.getDialogPane().getStyleClass().add("dialog-pane");
+        dialog.getDialogPane().getContent().getStyleClass().add("dialog-content");
+        dialog.getDialogPane().lookup(".header-panel").getStyleClass().add("dialog-header");
+        dialog.setTitle("dialog-title");
+
+        // Enable/Disable Ok button depending on whether password and confirm password are the same
+        Node okButton = dialog.getDialogPane().lookupButton(OkButton);
+        okButton.setDisable(true);
+
+        // Validate input
+        passwordField.textProperty().addListener((observable, oldValue, newValue) ->
+                okButton.setDisable(newValue.trim().isEmpty() || !newValue.equals(confirmPasswordField.getText())));
+        confirmPasswordField.textProperty().addListener((observable, oldValue, newValue) ->
+                okButton.setDisable(newValue.trim().isEmpty() || !newValue.equals(passwordField.getText())));
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == OkButton) {
+                if (passwordField.getText().equals(confirmPasswordField.getText()))
+                    return passwordField.getText();
+            }
+            return null;
+        });
+
+        dialog.showAndWait().ifPresent(result -> {
+            //ToDo:
+            // change password(result)
         });
     }
 
