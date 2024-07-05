8df666706aa3d563c60f28c00451da165573ee5a
alidd <moazenib.ali@gmail.com>
2024-07-05 19:12:31 +0330

    Add placeCard and vetoCard methods to GameMenuController

--- src/main/java/controller/GameMenuController.java
+++ src/main/java/controller/GameMenuController.java
@@ -5,61 +5,82 @@ import model.Card;
 import model.GameTable;
 import model.Player;
 import model.Result;
-import model.abilities.Decoy;
 
-import java.util.ArrayList;
+import java.util.Random;
 
 public class GameMenuController {
     public static GameTable currentGameTable;
     public static Player currentPlayer;
     public static Player opponentPlayer;
 
-    public Result vetoCard(int cardNumber) {
-
-        return new Result(true, "");
+    public void vetoCard(Card card) {
+        Random random = new Random();
+        Card cardToAdd = currentPlayer.getDeck().get(random.nextInt(0, currentPlayer.getDeck().size()));
+        currentPlayer.getHand().add(cardToAdd);
+        currentPlayer.getHand().remove(card);
     }
-    public String showDeck(){
+
+    public String showDeck() {
         return "";
     }
 
-    public Card randomChoose(ArrayList<Card> deck){
-        return null;
-    }
-
     public Result showInHand(int cardNumber) {
-        return new Result(true,"");
+        return new Result(true, "");
     }
 
-    public Result numOfRemainingCards() {
-        return new Result(true,"");
+    public Result getNumOfRemainingCards() {
+        return new Result(true, "");
     }
 
     public Result showDiscardPile() {
-        return new Result(true,"");
+        return new Result(true, "");
     }
 
     public Result showCardsInRow(int rowNumber) {
-        return new Result(true,"");
+        return new Result(true, "");
     }
 
     public Result showSpellInPlay() {
-        return new Result(true,"");
+        return new Result(true, "");
     }
 
-    public Result placeCard(int cardNumber, int roundNumber) {
-        return new Result(true,"");
+    public void placeCard(Card card, String row) {
+        if (row == null) {
+            currentGameTable.addToWeather(card);
+            return;
+        }
+        if (card.getType().equals("Special")) {
+        switch (row) {
+                case "Close Combat Unit":
+                    currentPlayer.getCloseCombat().addToCards(card);
+                case "Ranged Unit":
+                    currentPlayer.getRangedCombat().addToCards(card);
+                case "Siege Unit":
+                    currentPlayer.getSiege().addToCards(card);
+            }
+        }
+        else {
+            switch (row) {
+                case "Close Combat Unit":
+                    currentPlayer.getCloseCombat().setSpecial(card);
+                case "Ranged Unit":
+                    currentPlayer.getRangedCombat().setSpecial(card);
+                case "Siege Unit":
+                    currentPlayer.getSiege().setSpecial(card);
+            }
+        }
     }
 
-    public void doAction(Card card){
+    public void doAction(Card card) {
 
     }
 
-    public void reviveCard(Card card){
+    public void reviveCard(Card card) {
 
     }
 
     public Result showCommander() {
-        return new Result(true,"");
+        return new Result(true, "");
     }
 
     public void playCommanderPower() {
@@ -67,47 +88,47 @@ public class GameMenuController {
     }
 
     public Result showPlayersInfo() {
-        return new Result(true,"");
+        return new Result(true, "");
     }
 
     public Result showPlayersLives() {
-        return new Result(true,"");
+        return new Result(true, "");
     }
 
     public Result showNumberOfCardsInHand() {
-        return new Result(true,"");
+        return new Result(true, "");
     }
 
     public Result showTurnInfo() {
-        return new Result(true,"");
+        return new Result(true, "");
     }
 
     public Result showTotalScore() {
-        return new Result(true,"");
+        return new Result(true, "");
     }
 
     public Result showTotalScoreOfRow(int rowNumber) {
-        return new Result(true,"");
+        return new Result(true, "");
     }
 
     public void passRound() {
 
     }
 
-    public void endTurn(){
+    public void endTurn() {
 
     }
 
-    public void disCardSpells(){
+    public void disCardSpells() {
 
     }
 
-    public void endGame(){
+    public void endGame() {
 
     }
 
-    public int CalculatePlayersTotalScore(){
-        return  0;
+    public int CalculatePlayersTotalScore() {
+        return 0;
     }
 
 
